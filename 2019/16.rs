02b1033bec74ce1fcfa70c39897fedbe4cb4d6e6
F5XS <f5xs.0000a@gmail.com>
2022-12-18 01:23:13 +0800

    Fixed output for 2019/16

--- 2019/16.rs
+++ 2019/16.rs
@@ -1,8 +1,7 @@
 use std::io::BufRead as _;
-
 fn main() {
     let mut buffer = String::new();
-    std::io::stdin().lock().read_line(&mut buffer);
+    std::io::stdin().lock().read_line(&mut buffer).unwrap();
 
     let input = buffer
         .chars()
@@ -10,28 +9,59 @@ fn main() {
         .map(|x| x as i32)
         .collect::<Vec<_>>();
 
-    dbg!(fft(&*input, 100, 1));
-    dbg!(fft(&*repeated, 100, 10000));
+    let fft_1 = fft(&*input, 100)
+        .iter()
+        .take(8)
+        .rev()
+        .enumerate()
+        .map(|(i, x)| x * 10i32.pow(i as u32)).sum::<i32>();
+    eprintln!("Day 16.1: {}", fft_1);
+
+    let skipped_digits = input[0 .. 7]
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(exp, base)| *base as usize * 10usize.pow(exp as u32))
+        .sum::<usize>();
+
+    let input_len = input.len();
+    let repeated = input
+        .into_iter()
+        .cycle()
+        .take(input_len * 10000)
+        .collect::<Vec<_>>();
+
+    let fft_2 = fft(&*repeated, 100)
+        .iter()
+        .skip(skipped_digits)
+        .take(8)
+        .rev()
+        .enumerate()
+        .map(|(i, x)| x * 10i32.pow(i as u32)).sum::<i32>();
+    eprintln!("Day 16.2: {}", fft_2);
 }
 
-fn fft(input_array: &[i32], passes: usize, repetitions: usize) -> Vec<i32> {
+fn fft(input_array: &[i32], passes: usize) -> Vec<i32> {
     use std::cmp::min;
 
     let mut array_1 = vec![0; input_array.len()];
     let mut array_2 = vec![0; input_array.len()];
 
-    for i in 0 .. passes {
-        eprintln!("Pass #{}", i + 1);
+    let length = input_array.len();
+
+    for pass in 0 .. passes {
+        eprintln!("Pass {}", pass);
+
         let ref input;
         let ref mut output;
 
         // define where to put the output
-        if i == 0 {
+        if pass == 0 {
             input = input_array;
             output = &mut array_1;
         }
 
-        else if i % 2 == 1 {
+        else if pass % 2 == 1 {
             input = &array_1;
             output = &mut array_2;
         }
@@ -46,47 +76,71 @@ fn fft(input_array: &[i32], passes: usize, repetitions: usize) -> Vec<i32> {
             *x = 0;
         }
 
-        // populate the output
-        for (size, out) in output.iter_mut().enumerate() {
-            let size = size + 1;
-            eprintln!("Size #{}", size);
-            
-            for offset in 0 .. {
-                // add the positive portion
-                let positive_offset = size * (4 * offset + 1) - 1;
-                let positive_max = min(
-                    positive_offset + size,
-                    input_array.len()
-                );
+        // try to visualize the multiplier table (the -1, 0, and 1) using a
+        // visualizer
+        //
+        // if you try to notice an expanded form of the multiplier table, you'll
+        // notice bands like rays of light coming from the origin (top left of
+        // the table). you can utilize these multipliers in order to not
+        // needlessly sum what has been already summed. assume a single "band"
+        // then create a horizontal slice going from top to bottom. notice that
+        // as you go down, the band moves in a predictable way: removing from
+        // the left and adding from the right. perhaps you can exploit this.
 
-                if positive_offset >= input_array.len() {
+        for i in 0 .. length / 2 {
+            let mut current_sum = 0;
+            let mut starting_index = 0;
+            let mut ending_index = 0;
+
+            let multiplier = match i % 2 {
+                0 => 1,
+                1 => -1,
+                _ => unreachable!(),
+            };
+
+            for (n, output) in output.iter_mut().enumerate() {
+                // modify indices
+                let new_starting_index = n * (2 * i + 1) + 2 * i;
+                let new_ending_index = new_starting_index + n + 1;
+
+                // don't even consider those outside the bounds
+                if length <= new_starting_index {
                     break;
                 }
 
-                for value in &input[positive_offset .. positive_max] {
-                    *out += value;
+                // modify sum
+                if ending_index <= new_starting_index {
+                    current_sum = input[new_starting_index .. min(new_ending_index, length)]
+                        .iter()
+                        .sum();
                 }
 
-                // add the negative portion
-                let negative_offset = size * (4 * offset + 3) - 1;
-                let negative_max = min(
-                    negative_offset + size,
-                    input_array.len()
-                );
+                // this part is the optimization. it's a very great
+                // optimization.
+                else {
+                    // subtract those in the back
+                    current_sum -= input[starting_index .. min(new_starting_index, length)]
+                        .iter()
+                        .sum::<i32>();
 
-                if negative_offset >= input_array.len() {
-                    break;
+                    // and add those in the front
+                    if ending_index < length {
+                        current_sum += input[ending_index .. min(new_ending_index, length)]
+                            .iter()
+                            .sum::<i32>();
+                    }
                 }
 
-                for value in &input[negative_offset .. negative_max] {
-                    *out -= value;
-                }
+                // perform summation
+                *output += current_sum * multiplier;
+
+                starting_index = new_starting_index;
+                ending_index = new_ending_index;
             }
         }
 
-        // fix the output
-        for out in output.iter_mut() {
-            *out = out.abs() % 10;
+        for output in output.iter_mut() {
+            *output = output.abs() % 10;
         }
     }
 
